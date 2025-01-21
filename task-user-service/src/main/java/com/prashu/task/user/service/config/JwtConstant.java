fcf32614f5489e1d55cbeb0bb2133819c50cb833
Iris Sahu <prashusahu686@gmail.com>
2025-01-21 11:25:42 +0530

    first commit

---
+++ task-user-service/src/main/java/com/prashu/task/user/service/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.prashu.task.user.service.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "skldjfkhajskdhfkjjckjasdcklasjdkfjweijfksjdfkljsdkdjfakljsdk";
+    public static final String JWT_HEADER = "Authorization";
+}
